1fc47267e80e092d76330fbf6d99289eb6cb447c
Zalida-Khan <zkhan64@my.bcit.ca>
2024-11-29 11:16:18 -0800

    initial commit

--- .history/form-leaderboard-backend/models/Vote_20241125152332.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const voteSchema = new mongoose.Schema({
-  formName: { type: String, required: true },
-  count: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model("Vote", voteSchema);
